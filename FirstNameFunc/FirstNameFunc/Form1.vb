fde9db67aa0e14cea09542df1397ddf6db24179c
idsc3101 <idsc3101@gmail.com>
2013-10-07 17:00:07 -0500

    CLASS WORK

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -5,11 +5,26 @@ Public Class Form1
     Private Sub btnDetermine_Click(sender As System.Object, e As System.EventArgs) Handles btnDetermine.Click
         Dim name As String
         name = txtFullName.Text
+
+        Dim result As String
+        result = FirstName(name)
+        txtFirstName.Text = result
+
         txtFirstName.Text = FirstName(name)
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+
+        Dim idx As Integer
+        idx = name.IndexOf(" ")
+
+        Dim flame As String
+        flame = name.Substring(0, idx)
+
+
+        Return flame
+
+
     End Function
 
 End Class
